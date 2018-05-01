bd8de500c99221d7a4bdf0e2a2a4d0837679447f
Dea  V <506thinfantry@gmail.com>
2018-05-01 19:13:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,7 +21,3 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
-
-function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift()
-}
