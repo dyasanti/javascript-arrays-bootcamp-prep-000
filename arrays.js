8fd0a7f68e11b7113be13bfcce7736785ca8f2a4
Dea  V <506thinfantry@gmail.com>
2018-05-01 18:38:39 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,5 +2,5 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
   array = [];
-  return array.unshift(element)
+  return [...array, element];
 }
