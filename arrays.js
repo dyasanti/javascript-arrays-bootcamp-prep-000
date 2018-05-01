edcf44530c0e05c08226c94a3cf85a95da48cf3f
Dea  V <506thinfantry@gmail.com>
2018-05-01 18:30:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,1 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-
-function addElementToBeginningOfArray() {
-  var array = [];
-  var element = ()
-  return array.unshift(element)
-}
